4a933a12585702acbf8499124603329f19f18ac4
Amy <amyh102@gmail.com>
2022-03-19 19:13:43 -0400

    fix: remove useProjects hook

--- stories/ProjectListItem.stories.tsx
+++ stories/ProjectListItem.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react'
 import { ComponentMeta } from '@storybook/react'
 
@@ -12,7 +13,33 @@ const defaultProps = {
   title: 'My project',
   lastEdited: 'Mar. 10, 2022',
   nodes: 5,
-  views: 10,
+  visibility: 'public',
+  actionMenuRoot: {
+    onClick: () => {},
+    label: 'View/Publish',
+  },
+  actionMenuLast: {
+    onClick: () => {},
+    label: 'Delete',
+  },
+  actionMenuOptions: [
+    {
+      onClick: () => {},
+      label: 'Rename',
+    },
+    {
+      onClick: () => {},
+      label: 'Duplicate',
+    },
+    {
+      onClick: () => {},
+      label: 'Share',
+    },
+    {
+      onClick: () => {},
+      label: 'Rename',
+    },
+  ],
 }
 
 export const ProjectListItemSample = () => <ProjectListItem {...defaultProps} />
