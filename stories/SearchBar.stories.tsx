c94e9daace9ab038f80f9af7c75a3fd01c2b7324
Amyh102 <amyh102@gmail.com>
2022-03-19 16:16:31 -0400

    feat: searchbar component (#17)
    * Search bar component
    
    * Change background colour
    
    * move const
    
    * Remove unused theme
    
    * merge
    
    * fix: remove redundant styles
    
    Co-authored-by: H-Richard <r.hong@pm.me>

---
+++ stories/SearchBar.stories.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React from 'react'
+import { ComponentMeta } from '@storybook/react'
+
+import { SearchBar } from 'src/components'
+
+export default {
+  title: 'Components/SearchBar',
+  component: SearchBar,
+} as ComponentMeta<typeof SearchBar>
+
+const defaultProps = {
+  value: '',
+  onChange: () => {},
+}
+
+export const SearchBarSample = () => (
+  <div style={{ background: 'white', padding: '10px' }}>
+    <SearchBar {...defaultProps} />
+  </div>
+)
