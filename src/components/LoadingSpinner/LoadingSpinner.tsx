4de3a681645a4339813af891d1cc3142a5dfd3f4
Amy <amyh102@gmail.com>
2022-03-20 16:08:59 -0400

    feat: loading spinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,18 @@
+import className from 'classnames'
+
+import styles from './LoadingSpinner.module.scss'
+
+export const LoadingSpinner = () => {
+  return (
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
