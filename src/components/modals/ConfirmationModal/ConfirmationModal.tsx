9e5fd22cb9136df73953f6f779f5711af83c1399
jodijang <65675614+jodijang@users.noreply.github.com>
2022-03-28 22:03:05 -0400

    feat: add confirmation modal (#29)
    * feat: add confirmation modal
    
    * fix: mobile layout and styling

---
+++ src/components/modals/ConfirmationModal/ConfirmationModal.tsx
@@ -0,0 +1,51 @@
+import { useCallback, useContext } from 'react'
+
+import { IconButton, Button, ModalWrapper } from 'src/components'
+import { ModalContext } from 'src/contexts'
+import { close } from 'src/components/IconButton/icons'
+import styles from './ConfirmationModal.module.scss'
+
+export interface ConfirmationModalProps {
+  title: string
+  content?: string
+  onConfirm: VoidFunction
+}
+
+export const ConfirmationModal: React.FC<ConfirmationModalProps> = (
+  props: ConfirmationModalProps
+) => {
+  const { title, content, onConfirm } = props
+  const {
+    useConfirmationModalState: [, setConfirmationModalState],
+  } = useContext(ModalContext)
+
+  const handleCloseClick = useCallback(() => {
+    setConfirmationModalState(null)
+  }, [setConfirmationModalState])
+
+  return (
+    <ModalWrapper>
+      <div className={styles.container}>
+        <div className={styles.close}>
+          <IconButton onClick={handleCloseClick}>{close}</IconButton>
+        </div>
+        <h2>{title}</h2>
+        {content && <p>{content}</p>}
+        <div className={styles.buttons}>
+          <Button
+            label="Cancel"
+            variant="secondary"
+            onClick={handleCloseClick}
+          />
+          <Button
+            label="Confirm"
+            onClick={() => {
+              onConfirm()
+              handleCloseClick()
+            }}
+          />
+        </div>
+      </div>
+    </ModalWrapper>
+  )
+}
--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,3 +1,4 @@
 export * from './NewProjectModal'
 export * from './ErrorModal'
 export * from './LoadingModal'
+export * from './ConfirmationModal'
