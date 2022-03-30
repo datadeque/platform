9f2895c0c9c7bf75eda7454d6f6509650c044d1e
jodijang <joedjang@gmail.com>
2022-03-30 11:13:01 -0400

    feat: add sentry error monitoring

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const config = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com'],
   },
 }
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+module.exports = withSentryConfig(config, sentryWebpackPluginOptions)
