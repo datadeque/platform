124b02861285ed13a9ea493234b779dde6a397df
Amy <amyh102@gmail.com>
2022-03-30 00:55:27 -0400

    fix: merge conflicts

---
+++ src/components/modals/NewNodeModal/icons.tsx
@@ -0,0 +1,165 @@
+export const close = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path
+      fillRule="evenodd"
+      d="M5.72 5.72a.75.75 0 011.06 0L12 10.94l5.22-5.22a.75.75 0 111.06 1.06L13.06 12l5.22 5.22a.75.75 0 11-1.06 1.06L12 13.06l-5.22 5.22a.75.75 0 01-1.06-1.06L10.94 12 5.72 6.78a.75.75 0 010-1.06z"
+    />
+  </svg>
+)
+
+export const bar = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fillRule="evenodd"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeMiterlimit="1.5"
+    clipRule="evenodd"
+    viewBox="0 -1.5 2253 2253"
+  >
+    <path fill="none" d="M1.393 0H2251.393V2250H1.393z" />
+    <path
+      fill="#3050f3"
+      stroke="#000"
+      strokeWidth="66.67"
+      d="M1437.61 395.833H1750.11V1854.163H1437.61z"
+    />
+    <path
+      fill="#4dbe4b"
+      stroke="#000"
+      strokeWidth="66.67"
+      d="M397.226 1020.83H709.726V1854.163H397.226z"
+    />
+    <path
+      fill="#308df8"
+      stroke="#000"
+      strokeWidth="66.67"
+      d="M918.06 604.167H1230.56V1854.167H918.06z"
+    />
+    <path d="M288.893 387.5l-100-200-100 200h200zM1863.89 1962.5l200 100-200 100v-200z" />
+    <path
+      fill="none"
+      stroke="#000"
+      strokeWidth="66.67"
+      d="M188.893 347.5v1715h1715"
+    />
+  </svg>
+)
+
+export const line = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fillRule="evenodd"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeMiterlimit="1.5"
+    clipRule="evenodd"
+    viewBox="0 -1.5 2253 2253"
+  >
+    <path fill="none" d="M1.288 0H2251.288V2250H1.288z" />
+    <path d="M309.621 408.333l-100-200-100 200h200zM1884.62 1983.33l200 100-200 100v-200z" />
+    <path
+      fill="none"
+      stroke="#000"
+      strokeWidth="66.67"
+      d="M209.621 368.333v1715h1715"
+    />
+    <path
+      fill="none"
+      stroke="#4dbe4b"
+      strokeWidth="66.67"
+      d="M417.954 1875l625-625 208.334 416.667 625-625"
+    />
+    <path
+      fill="none"
+      stroke="#3050f3"
+      strokeWidth="66.67"
+      d="M417.954 1583.33l625-750 208.334 333.334 625-750"
+    />
+  </svg>
+)
+
+export const pie = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fillRule="evenodd"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeMiterlimit="1.5"
+    clipRule="evenodd"
+    viewBox="0 -1.5 2253 2253"
+  >
+    <path fill="none" d="M1.288 0H2251.288V2250H1.288z" />
+    <path
+      fill="#3050f3"
+      stroke="#000"
+      strokeWidth="66.67"
+      d="M1147.12 208.333c-517.767 0-937.5 419.733-937.5 937.5 0 428.307 290.275 802.155 705.228 908.271l232.272-908.271v-937.5z"
+    />
+    <path
+      fill="#4dbe4b"
+      stroke="#000"
+      strokeWidth="66.67"
+      d="M1996.79 749.576c-153.978-330.174-485.322-541.243-849.664-541.243v937.408l849.664-396.165z"
+    />
+    <path
+      fill="#308df8"
+      stroke="#000"
+      strokeWidth="66.67"
+      d="M904.478 2051.39c500.125 134.008 1014.19-162.788 1148.2-662.913a937.494 937.494 0 00-54.072-634.922l-851.483 392.279L904.48 2051.39z"
+    />
+  </svg>
+)
+
+export const scatter = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fillRule="evenodd"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeMiterlimit="1.5"
+    clipRule="evenodd"
+    viewBox="0 0 2253 2253"
+  >
+    <path fill="none" d="M0 0.295H2250V2250.295H0z" />
+    <path d="M287.5 387.795l-100-200-100 200h200zM1862.5 1962.8l200 100-200 100v-200z" />
+    <path
+      fill="none"
+      stroke="#000"
+      strokeWidth="66.67"
+      d="M187.5 347.795v1715h1715"
+    />
+    <circle
+      cx="600"
+      cy="1641.96"
+      r="100"
+      fill="#4dbe4b"
+      stroke="#000"
+      strokeWidth="66.67"
+    />
+    <circle
+      cx="895.833"
+      cy="979.462"
+      r="200"
+      fill="#308df8"
+      stroke="#000"
+      strokeWidth="66.67"
+    />
+    <circle
+      cx="1329.17"
+      cy="1229.46"
+      r="66.667"
+      fill="#4dbe4b"
+      stroke="#000"
+      strokeWidth="66.67"
+    />
+    <circle
+      cx="1645.83"
+      cy="812.795"
+      r="133.333"
+      fill="#3050f3"
+      stroke="#000"
+      strokeWidth="66.67"
+    />
+  </svg>
+)
